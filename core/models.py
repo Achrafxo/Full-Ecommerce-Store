b0658d72f80bd0aa75b4468a919da67138ebe75b
yaniskrn <yaniskorine@gmail.com>
2022-11-25 01:21:15 +0100

    added product image to database and render to template

--- core/models.py
+++ core/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=120, null=True)
     price = models.FloatField(null=True)
     description = models.TextField(max_length=1500, null=True)
-    #picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    picture = models.ImageField(null=True, blank=False, upload_to='images/')
     category = models.CharField(max_length=20, choices=choices)
     brand = models.CharField(max_length=20, null=True)
     color = models.CharField(max_length=20, null=True)
--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     path('dashboard/',views.Dashboard, name='dashboard'),
     path('contact/',views.Contact, name='contact'),
-    path('details/<str:pk>/', views.Product_Details, name='product details')
-    
+    path('details/<str:pk>/', views.Product_Details, name='product details'),
 ]
